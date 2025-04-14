8cb205f0047fdcdd8d57102e4cbabbc74d3b7a06
LTbirdyy <gabriel.fm.rosa@gmail.com>
2025-04-14 09:03:05 -0300

    Update Tarefa_14.py

--- .venv/Tarefa_14.py
+++ .venv/Tarefa_14.py
@@ -2,4 +2,4 @@
 n1 = int(input('Escolha um números para eu fazer a tabuada dele'))
 for c in range (1,11):
     resultado = n1 * c
-    print(f'{n1} x {c} = {resultado}')
+    print(f'{n1} x {c:2d} = {resultado:2d}')
