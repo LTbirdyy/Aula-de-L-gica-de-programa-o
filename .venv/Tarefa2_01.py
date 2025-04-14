9d80547a1833fb7f378c168a8a141bf7cc60fbbd
LTbirdyy <gabriel.fm.rosa@gmail.com>
2025-04-14 08:25:34 -0300

    Create Tarefa2_01.py

---
+++ .venv/Tarefa2_01.py
@@ -0,0 +1,13 @@
+cont_poositivo = 0
+total = 0
+
+print('Fale quantos números quiser para eu fazer a média deles, caso queira parar escreva algum número negativo:')
+while True:
+    n1 = int(input('Fale algum número:'))
+    if n1 < 0:
+        break
+    if n1 > 0:
+        cont_poositivo += 1
+        total += n1
+        media = total/cont_poositivo
+print(f'A média dos valores dados é {media}')
